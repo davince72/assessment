0b723bbd3957c611b9916061919a843208018100
davince <davince@davince-X555LAB>
2021-10-14 22:56:51 +0200

    some more

--- client-server/src/main/java/com/davince/OAuth2ClientApplication.java
+++ client-server/src/main/java/com/davince/OAuth2ClientApplication.java
@@ -1,11 +1,18 @@
 package com.davince;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.davince.model.CustomerRepository;
 
 @SpringBootApplication
+//@ComponentScan({"com.davince"})
 public class OAuth2ClientApplication {
 
+	@Autowired
+	CustomerRepository repository;
     public static void main(String[] args) {
         SpringApplication.run(OAuth2ClientApplication.class, args);
     }
---
+++ client-server/src/test/java/com/davince/OAuth2ClientApplicationTest.java
@@ -0,0 +1,13 @@
+package com.davince;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ClientApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
