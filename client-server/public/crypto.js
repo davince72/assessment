ee3f2a22ec3f2d4289b9a5e82d589ce8ea15f142
davince <davince@davince-X555LAB>
2021-10-21 21:07:35 +0200

    last things

--- client-server/public/crypto.js
+++ client-server/public/crypto.js
@@ -1,20 +1,3 @@
-/*$(document).ready(function() {
-    $.ajax({
-        url: "http://127.0.0.1:8080/greetings.json" 
-    }).then(function(data) {
-       $('.coinid').append(data.id);
-       $('pricepoint').append(data.time.updated);
-    });
-});*/
-
-/*$(document).ready(function() {
-    $.ajax({
-        url: "https://api.coindesk.com/v1/bpi/currentprice.json"  
-    }).then(function(data) {
-       $('.coinid').append(data.disclaimer);
-       $('pricepoint').append(data.time.updated);
-    });
-});*/
 
 $(document).ready(function() {
     $.ajax({
@@ -26,12 +9,3 @@ $('.sellprice').append(data[0].sellPrice.amount);
     });
 });
 
-
-/*$(document).ready(function() {
-    $.ajax({
-        url: "http://127.0.0.1:8080/coin.json"  
-    }).then(function(data) {
-       $('.coinid').append(data.disclaimer);
-       $('pricepoint').append(data.time.updated);
-    });
-});*/
